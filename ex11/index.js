29af08c4040d9075f51cce299414c9013b0cd343
danilo-sds <danilosouza302@gmail.com>
2024-09-05 00:13:31 -0300

    Add Ex11

--- ex11/index.js
+++ ex11/index.js
@@ -1,12 +1,17 @@
 //Escreva uma função que verifica se um número fornecido pelo usuário em um prompt é primo ou não.
-let numberInfo = parseInt(prompt("Digite um número: "));
 
-function verificarNumero(num) {
+let numberClient = parseInt(
+  prompt("Digite um número para verificar se é primo")
+);
+
+function verificarNumeroPrimo(num) {
   if (num % 2 == 1) {
-    return num;
+    alert(`O número é primo!`);
+  } else if (num % 2 == 0) {
+    alert(`O número é par!`);
+  } else {
+    alert(`Por favor, informe um número certo!`);
   }
 }
 
-let result = verificarNumero(numberInfo);
-
-console.log(result);
+verificarNumeroPrimo(numberClient);
